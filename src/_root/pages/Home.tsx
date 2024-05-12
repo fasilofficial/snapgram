35725053fe92e0c58399f4e903740a743ed0e02d
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-05-12 15:41:25 +0530

    Implement create-post form and create post functionality

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    data: posts,
+    isPending: isPostsLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostsLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <li key={post.$id}>{post.caption}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -11,3 +11,10 @@ export const SigninValidationSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Password is too short" }),
 });
+
+export const postValidationSchema = z.object({
+  caption: z.string().min(2).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
